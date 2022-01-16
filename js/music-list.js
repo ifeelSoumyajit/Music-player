d2a8e329ba0f1c8aa00a4f9c3fda3567018bf7f2
Soumyajit Mandal <95968392+iSoumyajitMandal@users.noreply.github.com>
2022-01-16 12:51:14 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+  {
+    name: "Alec Benjamin - Let Me Down Slowly",
+    artist: "Alec Benjamin",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Fifth Harmony - Worth It",
+    artist: "Fifth Harmony",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Billie Eilish, Khalid - lovely",
+    artist: "Billie Eilish, Khalid",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Ariana Grande - Into You",
+    artist: "Ariana Grande",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Duncan Laurence - Arcade",
+    artist: "Duncan Laurence",
+    img: "music-5",
+    src: "music-5"
+  },
+];
